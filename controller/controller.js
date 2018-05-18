bd59b803d09bfe752e7a8c1d6b2c348778ce4d43
amber phillips <amberphillips@ambers-MacBook-Pro.local>
2018-05-17 22:21:58 -0500

    added 2 files to models

--- controller/controller.js
+++ controller/controller.js
@@ -1,15 +1,26 @@
 let express = require("express");
 let router = express.Router();
-let models = require("../models/chain.js");
+let models = require("../models/chain.js");    ///// not sure if this needs to be var or since not since calling globally 
+var character = require("../models/character.js");    
+var user = require("../models/user.js")
 
-router.get("/", (req, res) => {
-    res.redirect("/simpsons");
-});
+///// need to pull from character data based on the character.js file
+module.exports = function(app) {
+    /// get function to pull a character based on user input 
+    app.get("/api/all", function(req, res) {
+      character.findAll({}).then(function(results) {
+        res.json(results);
+      });
+    });
+  
+///// need to add another get for the user here
 
-router.get("/simpsons", (req, res) => {
 
-    res.render("index", {});
-});
+///// need to post the character
 
+/// need to post maybe username??
 
-module.exports = router;
+/// need to post an image
+
+//// loop through randomizing characters or create a seperate js file??
+
