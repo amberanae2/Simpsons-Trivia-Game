a4e7f228eb0e1908b665244c2582052d150a74aa
aaronmcma49 <aaronmcmanus49@gmail.com>
2018-05-21 21:30:18 -0500

    adding logic for grabbing 1 random row from db

--- controller/controller.js
+++ controller/controller.js
@@ -7,8 +7,17 @@ var user = require("../models/user.js")
 
 //// need to route to testHtml
 
-//// router.get("/", function(req, res) { 
-////   res.json(testHtml);
+router.get("/", function(req, res) {
+    res.redirect("/simpsonTriviaLogin"); //this redirects us to the route below.
+   });
 
-
-//// 
+   
+   router.get("/simpsonTriviaLogin", function(req, res) {
+    // express callback response by calling burger.selectAllBurger
+   //SEQUELIZE code to pull data from database using the information gathered from the login page.
+      // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+      res.render("trivia", { game_data: gameData }); 
+   // game data is a variable I made up. It’s being used in the imaginary Sequelize code. 
+    });
+   
+   
