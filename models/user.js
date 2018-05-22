23360f0242e70413376aa9cf475c6e45afa37e30
amber phillips <amberphillips@ambers-MacBook-Pro.local>
2018-05-21 19:38:14 -0500

    added passport/modified routes

--- models/user.js
+++ models/user.js
@@ -1,13 +1,8 @@
 module.exports = function(sequelize, DataTypes) {
-    var User = sequelize.define("User", {
+    var User = sequelize.define("user", {
       Username: DataTypes.STRING,
       password: DataTypes.STRING
       //// not sure if I need to add donut coins here since it is part of the table.
     });
     return User;
   };
-<<<<<<< HEAD
-  
-=======
-
->>>>>>> 02716fb7050f70b363a56e0d6c5101c6fcc36c34
