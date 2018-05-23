0013f866251db694817ea7711a6b293b13528b04
amber phillips <amberphillips@ambers-MacBook-Pro.local>
2018-05-22 20:54:40 -0500

    deleted a file

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
-// module.exports = function(sequelize, DataTypes) {
-//     var User = sequelize.define("user", {
-//       Username: DataTypes.STRING,
-//       password: DataTypes.STRING
-//       //// not sure if I need to add donut coins here since it is part of the table.
-//     });
-//     return User;
-//   };
+var Character = require("");
+var User = require("");
+var Sequelize = require("sequelize");
+var sequelize = require("../config/connection.js");
+
+
+sequelize.query('SELECT * FROM characters', { model: characters }).then(characters => {
+
+
+})
